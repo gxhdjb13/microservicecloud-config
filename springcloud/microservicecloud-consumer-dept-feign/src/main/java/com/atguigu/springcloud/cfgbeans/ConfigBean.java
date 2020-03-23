760acead48c0be9535971069ee1b868d75eaa39d
gxh <xuhui.gao@jiangrongxin.com>
2020-03-23 12:27:37 +0800

    提交springcloud代码

---
+++ springcloud/microservicecloud-consumer-dept-feign/src/main/java/com/atguigu/springcloud/cfgbeans/ConfigBean.java
@@ -0,0 +1,28 @@
+package com.atguigu.springcloud.cfgbeans;
+
+import com.netflix.loadbalancer.IRule;
+import com.netflix.loadbalancer.RetryRule;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+public class ConfigBean {
+    @Bean
+    @LoadBalanced  //Spring Cloud Ribbon是基于Netflix Ribbon实现的一套客户端 负载均衡的工具.
+    public RestTemplate getRestTemplate(){
+        return new RestTemplate();
+    }
+
+    //随机机制
+//    @Bean
+//    public IRule myRule(){
+//        return new RandomRule();//用我们选择的随机算法替代默认的轮询
+//    }
+    //重试机制
+    @Bean
+    public IRule myRule(){
+        return new RetryRule();
+    }
+}
